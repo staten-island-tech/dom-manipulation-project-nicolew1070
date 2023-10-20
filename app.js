d98cfd826660f3b5420b00b122b0a5439cf6a0a6
nicolew88 <nicolew88@nycstudents.net>
2023-10-20 13:13:06 -0400

    .

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-DOMselectors = {
+DOMSelectors = {
     form: document.querySelector("#form"),
-    firstName: document.querySelector("firstName"),
+    firstName: document.querySelector(".first-name"),
+    h2s: document.querySelector("h2"),
+};
 
-}
+DOMSelectors.form.addEventListener("submit", function (event) {
+    event.preventDefault(),
+    DOMSelectors.h2s.forEach(
+    (header) => (header.textContent = DOMSelectors.firstName.value));
+    }
+);
